9656640073b636b91c0fbfb1d4a047f0cdcbefc7
Tieshun Roquerre <tieshun@teespring.com>
2015-04-27 16:32:57 -0700

    Implement Payoneer GetToken as Payoneer::Payee.signup_url

---
+++ spec/payoneer/payee_spec.rb
@@ -0,0 +1,51 @@
+require 'spec_helper'
+
+describe Payoneer::Payee do
+  describe '.signup_url' do
+    context 'when Payoneer successfully returns a signup url' do
+      it 'returns a success response with the signup url' do
+        signup_url = 'http://www.payoneer.com/signupurl'
+        successful_response = { "Token" => signup_url }
+        payee_id = 'payee123'
+        redirect_url = 'redirect'
+        redirect_time = 'quarter-til'
+        return_xml = true
+
+        expect(Payoneer).to receive(:make_api_request).
+          with('GetToken', {p4: payee_id, p6: redirect_url, p8: redirect_time, p10: return_xml}).
+          and_return(successful_response)
+
+        expected_response = Payoneer::Response.new('000', signup_url)
+        actual_response = described_class.signup_url(
+          payee_id,
+          redirect_url: redirect_url,
+          redirect_time: redirect_time,
+        )
+
+        expect(actual_response).to eq(expected_response)
+      end
+    end
+
+    context 'when Payoneer returns an error response' do
+      let(:error_code) { '001' }
+      let(:error_description) { 'Field format is invalid or is not supported' }
+      let(:error_response) {
+        {
+          "Description" => error_description,
+          "Code" => error_code,
+        }
+      }
+
+      before do
+        allow(Payoneer).to receive(:make_api_request) { error_response }
+      end
+
+      it 'raises a BadResponseStatusError' do
+        error_response = Payoneer::Response.new(error_code, error_description)
+
+        expect(described_class.signup_url('payee123')).to eq(error_response)
+        expect(error_response).to_not be_ok
+      end
+    end
+  end
+end
