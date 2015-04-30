1b8cd14d0c01f64274a44e0a026d7e9210ed6754
Kevin Liddle <kevin.liddle@teespring.com>
2015-04-30 11:31:38 -0500

    Add webmock so tests don't hit the real Payoneer

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,7 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'payoneer'
 require 'pry'
+require 'webmock/rspec'
 
 RSpec.configure do |config|
   config.mock_with :rspec do |mocks|
